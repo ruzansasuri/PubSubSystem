dfcd29e720415756a6b3418cbb9c2ce58bf09688
ruzansasuri <ruzansasuri@gmail.com>
2018-02-05 23:23:14 -0500

    got that gson b**** out of the way

--- pubsub.sh
+++ pubsub.sh
@@ -1,2 +1,2 @@
-gnome-terminal -e "docker run --rm -it -cp lib/gson-2.6.2.jar pubsub_sub"
-gnome-terminal -e "docker run --rm -it -cp lib/gson-2.6.2.jar pubsub_em"
+gnome-terminal -e "docker run --rm -it pubsub_em"
+gnome-terminal -e "docker run --rm -it pubsub_sub"
